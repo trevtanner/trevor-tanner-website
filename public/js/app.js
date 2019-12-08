b6343593c7f16b88e22b0c6639c38effa6f8ef6f
trevtanner <trevtanner@gmail.com>
2019-12-08 01:31:23 -0800

    Slimming JS code

--- public/js/app.js
+++ public/js/app.js
@@ -49210,6 +49210,10 @@ Vue.component('example-component', __webpack_require__(/*! ./components/ExampleC
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+var app = new Vue({
+  el: '#app'
+});
+
 /***/ }),
 
 /***/ "./resources/js/bootstrap.js":
